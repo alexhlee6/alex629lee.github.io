749d431d2592211cae6d7a6881a48dccd1f80697
alex629lee <alex629lee@yahoo.com>
2020-01-17 14:06:36 -0800

    Add project descriptions under images

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
+const projectNames = {
+  1: {
+    name: "DokiCupid",
+    live: "http://dokicupid.herokuapp.com",
+    repo: "https://github.com/alex629lee/DokiCupidApp",
+    technologies: "React-Redux, Ruby on Rails, Postgres, AWS, WebSockets, AJAX",
+    description: "Anime-inspired dating app based on OkCupid featuring profiles, matchmaking, filtered search, and real-time chat."
+  },
+  2: {
+    name: "Yuki's Guitar",
+    live: "http://yukisguitar.herokuapp.com",
+    repo: "https://github.com/alex629lee/YukisGuitar",
+    technologies: "JavaScript, React, Canvas, jQuery, MongoDB, Express",
+    description: "Responsive music rhythm game similar to Guitar Hero where players score points by clicking buttons to the timing of the music."
+  },
+  3: {
+    name: "SpotToCry",
+    live: "http://spottocry.herokuapp.com",
+    repo: "https://github.com/alex629lee/SpotToCry",
+    technologies: "JavaScript, React, Apollo, GraphQL, MongoDB, Node.js, Express",
+    description: "Music streaming web application based on Spotify where users can ride out their melancholic moods."
+  },
+  // 4: {
+  //   name: "Synthagram",
+  //   live: "http://synthagram.herokuapp.com",
+  //   repo: "https://github.com/eliraybon/synthagram",
+  //   technologies: "",
+  //   description: ""
+  // },
+  4: {
+    name: "Netflex",
+    live: "http://netflex80s.herokuapp.com",
+    repo: "https://github.com/mitchellreiss/netflex",
+    technologies: "React, Redux, Node.js, Express, MongoDB, BCrypt, Validator",
+    description: "Retro workout-themed video streaming service based on Netflix for tubular time travelers to get movin' and groovin'."
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   let slides = document.getElementsByClassName("mySlides");
   slides[0].style.display = "block";
@@ -13,6 +51,10 @@ document.addEventListener("DOMContentLoaded", () => {
         </a>
       </h2>
     </span>
+  `;
+  document.getElementById("current-slide-description").innerHTML = `
+    <p><i>${projectNames[1].technologies}</i></p>
+    <p>${projectNames[1].description}</p>
   `;
 })
 var slideIndex = 1;
@@ -30,6 +72,7 @@ function currentSlide(n) {
 
 
 
+
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
@@ -39,34 +82,6 @@ function showSlides(n) {
   if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-  }
-
-  const projectNames = {
-    1: {
-      name: "DokiCupid",
-      live: "http://dokicupid.herokuapp.com",
-      repo: "https://github.com/alex629lee/DokiCupidApp"
-    },
-    2: {
-      name: "Yuki's Guitar",
-      live: "http://yukisguitar.herokuapp.com",
-      repo: "https://github.com/alex629lee/YukisGuitar"
-    },
-    3: {
-      name: "SpotToCry",
-      live: "http://spottocry.herokuapp.com",
-      repo: "https://github.com/alex629lee/SpotToCry"
-    },
-    4: {
-      name: "Synthagram",
-      live: "http://synthagram.herokuapp.com",
-      repo: "https://github.com/eliraybon/synthagram"
-    },
-    5: {
-      name: "Netflex",
-      live: "http://netflex80s.herokuapp.com",
-      repo: "https://github.com/mitchellreiss/netflex"
-    }
   }
 
   slides[slideIndex - 1].style.display = "block";
@@ -79,4 +94,9 @@ function showSlides(n) {
     </div>
   </h2>`;
   document.getElementById("current-slide-info").innerHTML = inner;
+
+  document.getElementById("current-slide-description").innerHTML = `
+    <p><i>${projectNames[slideIndex].technologies}</i></p>
+    <p>${projectNames[slideIndex].description}</p>
+  `;
 }
